6df5e3791080bdac2921f0c340064f1d9393e071
Ojas Srivastava <ojas1807@gmail.com>
2024-11-23 18:01:32 -0500

    Added fetchHp component

---
+++ app/components/fetchHp.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+interface APIComponentProps {
+  productID: number; // The API URL
+  onFetchComplete: (result: number) => void;
+}
+
+const FetchHp: React.FC<APIComponentProps> = ({ productID, onFetchComplete }) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/freestyle/' + productID.toString());
+        if (!response.ok) throw new Error('Failed to fetch data');
+        const data = await response.json();
+        const n = Math.floor(data['Progress Value']);
+        console.log(n);
+        onFetchComplete(n); // Call the parent function with `n`
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        onFetchComplete(-1); 
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return null; 
+};
+
+export default FetchHp;
