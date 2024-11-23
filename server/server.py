bc319a3adfcebb8beb1f0c23af795d222bf8a136
Ojas Srivastava <ojas1807@gmail.com>
2024-11-23 17:47:33 -0500

    Server CORS updated

--- server/server.py
+++ server/server.py
@@ -1,11 +1,20 @@
 from typing import Union
 from freestyle import HealthScoreByID, Stack, score_stack
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
 
 health_stack = Stack()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, change to specific domains for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
