8f238c98a598493d3310b036eb0c5894f29ca422
Sathya Sriram <98763491+sathyasriram777@users.noreply.github.com>
2024-11-24 09:04:42 -0500

    american

---
+++ app/components/fetchCal.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+interface APIComponentProps {
+    onFetchComplete: (calories : string) => void;
+}
+
+const FetchCal: React.FC<APIComponentProps> = ({ onFetchComplete }) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/american/');
+        if (!response.ok) throw new Error('Failed to fetch data');
+        const data = await response.json();
+        const calories = data;
+        console.log(calories);
+        onFetchComplete(calories);
+      } catch (error) {
+        console.error('Error fetching data:', error); 
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return null; 
+};
+
+export default FetchCal;
